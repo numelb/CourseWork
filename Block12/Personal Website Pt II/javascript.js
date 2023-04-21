be904f26f50a7723596f3908ed0244faf1f65b9f
numelb <numelb@gmail.com>
2023-04-21 09:00:27 -0500

    updated contact form in Block12

--- Block12/Personal Website Pt II/javascript.js
+++ Block12/Personal Website Pt II/javascript.js
@@ -2,11 +2,7 @@
 
 const form = document.querySelector("#form")
    const submitButton = document.querySelector("#submit")
-<<<<<<< HEAD
-   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
-=======
    const scriptURL = 'https://script.google.com/macros/s/AKfycbxLZTlgHBunzjDAyvzTFB9AAQZS2vGT4hSZKlfMDVMQrrHEQwKDYO2-fo05BNfFegYWVg/exec'
->>>>>>> updated unit 12,13 and added unit 14
 
    form.addEventListener('submit', e => {
      submitButton.disabled = true
