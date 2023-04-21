89b46a4e188ee1a0e34a39c0be8125afaed96393
numelb <numelb@gmail.com>
2023-04-21 10:00:01 -0500

    added index.js GreaterThan 5

---
+++ UNIT2/Block13/GreaterThan5/index.js
@@ -0,0 +1,18 @@
+// Using if/else statements, JavaScript code in a script tag that 
+// prints true if both parameters are greater or equal to 5.
+// create two variables num1 and num2
+// check if both num1 are greater or equal to five
+// print result in console
+
+
+let num1 =5;
+let num2 =5;
+
+if(num1 >= 5 && num2 >=5){  
+    console.log(num1 + " and " +num2)
+    console.log(true)   
+}
+else{
+    console.log(num1 + " and " +num2)
+    console.log(false)
+}
