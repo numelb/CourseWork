d105d0cebbfc8a25b38c2172864f2d235be51ca8
numelb <numelb@gmail.com>
2023-04-24 21:59:09 -0500

    tested code in OnlyOdds

--- UNIT2/Block14/OnlyOdds/index.js
+++ UNIT2/Block14/OnlyOdds/index.js
@@ -8,8 +8,8 @@
 // Test values
 // const oddArray = [2, 4, 6, 8, 11, 20, 15, 22]
 // const oddArray =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// const oddArray =[70, 42, 55, 81, 21, 91, 34]
-const oddArray = [2, 4, 6, 8, 10, 11, 12] 
+const oddArray =[70, 42, 55, 81, 21, 91, 34]
+// const oddArray = [2, 4, 6, 8, 10, 11, 12] 
 for (let i = 0; i< oddArray.length; i++){
     
         if(oddArray[i]%2 ===1){
