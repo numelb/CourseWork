32ac4408035884948d897d1b548d107718a1ca98
numelb <numelb@gmail.com>
2023-07-31 20:52:47 -0500

    Block27 workshop

---
+++ UNIT3/Block27/reactForms/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Authenticate from "./components/Authenticate.jsx";
+import SignUpForm from "./components/SignupForm.jsx";
+
+export default function App() {
+  
+
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  )
+}
+
+ 
